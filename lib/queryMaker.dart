35c37f21481a3cc01ecdff93dd9543633821d9c1
박슬우 <yhsyl2227@taehwainno.com>
2021-07-30 12:54:08 +0900

    annotation amended

--- lib/queryMaker.dart
+++ lib/queryMaker.dart
@@ -14,15 +14,19 @@ class QueryMaker {
     List<CheckboxMemo> case4list = [];
 
 
-    final database = openDatabase(
+    final database = openDatabase(///--------------------------------------------------------------------------------------------------------------------------------//
       join(await getDatabasesPath(),'checkboxmemo_database.db'),
       onCreate: (db, version){
+        print("db 생성됨");
         return db.execute('CREATE TABLE checkboxmemo(id INTEGER PRIMARY KEY, memotitle TEXT, memocontext TEXT, whatmatrix INTEGER, ischecked INTEGER)',);
       },
       version: 1,
     );
+    //더미데이터 삽입(실제로 넣는 부분 만들기 전까지만 사용)
 
-    ///get query in wholeEisenCheckboxMemo
+    //전체 디비 쿼리
+
+    /// 여기서 디비 생성 (또는 연결) 하고, 체크박스 디비를 전부 쿼리하여 wholeEisenCheckboxMemo 리스트에 넣음.-------------------------------------------------------------//
 
 
 
